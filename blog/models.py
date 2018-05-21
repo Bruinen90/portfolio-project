ca558cc3cd19d5b90f739f0db6352335cfa134aa
bruinen <max.machnik@gmail.com>
2018-05-21 21:41:32 +0200

    Push to server

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%d %B %Y')
